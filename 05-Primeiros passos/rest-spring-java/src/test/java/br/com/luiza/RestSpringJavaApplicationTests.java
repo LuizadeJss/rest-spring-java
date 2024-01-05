b84016501a98cd7f8883e3471deab8bc99808f46
LuizadeJss <ljff_luz@hotmail.com>
2024-01-05 19:13:07 -0300

    Primeiros passos em java com spring

---
+++ 05-Primeiros passos/rest-spring-java/src/test/java/br/com/luiza/RestSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luiza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
